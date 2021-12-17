5608201f09fd49a27c35e211c1d739462ce366a5
songjing <songjingjing.a@mininglamp.com>
2021-12-17 21:03:16 +0800

    feat: prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-    trailingComma: 'es5',
     tabWidth: 4,
-    semi: false,
+    // 单行代码最长字符长度，超过之后会自动格式化换行
+    printWidth: 120,
+    bracketSpacing: true,
     singleQuote: true,
+    // 在对象中的括号之间加空格， {a: 5} 格式化为 { a: 5 }
+    bracketSpacing: true,
+    // 箭头函数的参数无论有几个，都要括号包裹。比如 (a) => {} ，如果设为 avoid ，会自动格式化为 a => {}
+    arrowParens: true,
 }
