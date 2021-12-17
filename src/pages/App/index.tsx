9ba837fa1355677d768019d77e8d0cd0646ab4f2
songjing <songjingjing.a@mininglamp.com>
2021-12-17 14:58:06 +0800

    feat: App

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home'
+import Home from "../Home";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div>
-        <h1>Hello, React Router!</h1>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 }
