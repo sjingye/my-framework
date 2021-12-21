e2c6ad0cd4026a3e41faaeecf99dc1155274bf93
songjing <songjingjing.a@mininglamp.com>
2021-12-21 18:29:39 +0800

    feat: add cross-env

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,9 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
-process.env.BABEL_ENV = "development";
-process.env.NODE_ENV = "development";
-
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,9 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
-
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
