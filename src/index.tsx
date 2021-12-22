aeceb8bce4bdeae00d2d2e138d66df96b6f74a5d
songjing <songjingjing.a@mininglamp.com>
2021-12-22 17:48:08 +0800

    feat: hotModulePlugin

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom'
 
 import App from './pages/App'
 
+// 这个时候，你 npm run start 并尝试改变局部的代码，保存后发现整个页面还是会进行刷新，
+// 如果你希望得到上面所说的“局部刷新”，需要在项目入口文件加以下代码。
+// npm install --save-dev @types/webpack-env
+if (module?.hot) {
+    module.hot.accept()
+  }
+
 ReactDOM.render(<App />, document.getElementById('root'))
